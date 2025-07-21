bd2cd0a4e1adbd12a16b732da42f7c092ad2e292
RedCaro14 <redcaro14@gmail.com>
2025-07-21 07:28:40 -0500

    fix: framer-motion config in vite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['framer-motion'], // Fuerza la inclusión en el pre-bundling
-    exclude: [], // Limpia exclusiones innecesarias
-  },
-  build: {
-    rollupOptions: {
-      external: [], // Elimina la externalización de framer-motion
-      output: {
-        manualChunks: {
-          // Separa framer-motion en su propio chunk (opcional para optimización)
-          'framer-motion': ['framer-motion'],
-        },
-      },
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true, // Crucial para módulos híbridos
-    },
-  },
-});
+})
